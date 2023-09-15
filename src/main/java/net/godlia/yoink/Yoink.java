dc8f9e07146629bbe324d07b98b325e7ae52f649
Godlia <52934844+Godlia@users.noreply.github.com>
2023-09-15 16:05:51 +0200

    Test

--- src/main/java/net/godlia/yoink/Yoink.java
+++ src/main/java/net/godlia/yoink/Yoink.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//test
 public class Yoink implements ModInitializer {
 	public static final String MOD_ID = "yoink";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
