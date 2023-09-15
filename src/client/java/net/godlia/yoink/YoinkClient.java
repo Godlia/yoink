bd16df5a89b6c5294f3f14f783281487df527ec2
Godlia <52934844+Godlia@users.noreply.github.com>
2023-09-15 15:57:45 +0200

    init

---
+++ src/client/java/net/godlia/yoink/YoinkClient.java
@@ -0,0 +1,10 @@
+package net.godlia.yoink;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YoinkClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/godlia/yoink/Yoink.java
@@ -0,0 +1,15 @@
+package net.godlia.yoink;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Yoink implements ModInitializer {
+	public static final String MOD_ID = "yoink";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
